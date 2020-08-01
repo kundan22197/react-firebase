69cb9eeb33d691fac78e087ba6dde07e941628e2
Kundan Patel <kundanpatel@kundans-mbp.myfiosgateway.com>
2020-08-01 15:08:05 -0400

    React-firebase-basic

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyCKtr2Q7hy5Bw5ZdvyY88EUO_86FQgpqTg",
+    authDomain: "react-firebase-authentic-662ff.firebaseapp.com",
+    databaseURL: "https://react-firebase-authentic-662ff.firebaseio.com",
+    projectId: "react-firebase-authentic-662ff",
+    storageBucket: "react-firebase-authentic-662ff.appspot.com",
+    messagingSenderId: "785981675653",
+}
